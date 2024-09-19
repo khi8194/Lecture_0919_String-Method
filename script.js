44a69496846da9c26cd58cce2a0c3c7014d41181
khi8194 <khi8194@gmail.com>
2024-09-19 15:43:10 +0900

    문자열을 특정 구분자로 문자열 이어붙이기, 특정 구분자만 변경처리하기, trim을 추가해서 불필요한 여백을 제거

--- script.js
+++ script.js
@@ -1,52 +1,35 @@
-console.log("Test");
+//전체 문자열에서 특정 문자값을 기준으로 분리해서 배열로 반환하는 내장함수
+const span_text = document.querySelector("span").innerText;
+console.log(span_text); //'2024-09-19'
 
-const h1 = document.querySelector("h1");
-h1.innerText = "Test";
+const text_arr = span_text.split("-");
+console.log(text_arr); //['2024','09','19];
 
-const h2 = document.querySelector("h2");
-const linkURL = "https://www.naver.com";
-h2.innerHTML = `<a href=${linkURL}>naver</a>`;
+//배열값은 특정 문자열로 이어서 하나의 문자열로 반환하는 함수
+const new_text = text_arr.join(".");
+console.log(new_text); //'2024.09.19'
 
-const h3 = document.querySelector("h3");
-console.log(h3.innerText.length);
+//위의 split과 join을 한번에 연산처리
+const h2_text = document.querySelector("h2").innerText;
+const new_h2 = h2_text.split(" ").join("/");
+console.log(new_h2);
 
-const h3_text = h3.innerText;
-//h3문자열을 0위치에서부터 50번째 위치까지 자른 뒤 자른 문자열을 다시 h3.innerText 프로퍼티에 대입해서 적용
-h3.innerText = h3_text.substring(0, 50);
+//미션 - 위의 구문을 다음과 같은 형태의 함수로 패키징
+//changeText('h2', ' ', '/')
 
-//미션 - 조건문을 활용해서 h3에 들어가는 문자값이 50글자가 안되면 해당 문자를 그대로 출력하고 50글자가 넘어가면 50글자까지 자른 뒤 그 뒤에 ... 말줄임표 추가
-const h4 = document.querySelector("h4");
-console.log(h4.innerText.length);
-
-const h4_text = h4.innerText;
-h4.innerText = h4_text.substring(0, 50);
-//이하 3가지 다 같은 출력을 갖는 코드
-// if (h4_text.length > 50) {
-// 	h4.innerText = h4_text.substring(0, 50) + "...";
-// } else {
-// 	h4.innerText = h4_text;
+// function changeText(el, spc1, spc2) {
+// 	const el_text = document.querySelector(el).innerText;
+// 	const new_el = el_text.split(spc1).join(spc2);
+// 	console.log(new_el);
 // }
 
-// if (h4_text.length > 50) h4.innerText = h4_text.substring(0, 50) + "...";
-// else h4.innerText = h4_text;
-
-// h4.innerText = h4_text.length > 30 ? h4_text.substring(0, 30) + "..." : h4_text;
-
-//미션 - 위의 기능을 재사용하기 편하도록 함수로 패키징
-shortenText("h1", 2);
-shortenText("h2", 3);
-shortenText("h3", 20);
-shortenText("h4", 30);
-
-function shortenText(elem, len) {
-	//첫번째 인수로 받은 문자값으로 DOM요소 선택
+function changeText(elem, spc1, spc2) {
 	const el = document.querySelector(elem);
-	//해당요소의 문자값 변수에 할당
-	const el_text = el.innerText;
-
-	// const len = el.innerText.length;
-
-	//해당 돔요소에 innerText프로퍼티에 대입되는 우항을 3항연산자로 연산 처리 후 대입
-	el.innerText =
-		el_text.length > len ? el_text.substring(0, len) + "..." : el_text;
+	//문자열.trim() : 문자열 앞뒤의 빈칸을 제거해서 정리
+	const el_text = el.innerText.trim();
+	// console.log(el_text);
+	el.innerText = el_text.split(spc1).join(spc2);
 }
+changeText("span", "-", ".");
+changeText("h2", " ", " / ");
+changeText("h3", "_", "-");
